a0c2ec2837b2c32b975ad2fc71c624be729c60dc
stanleya1293 <astanle8@ut.utm.edu>
2023-09-08 18:01:20 -0500

    updated main.cpp to a summation program

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
+int sum(int n) {
+  int sum = 0;
+  for(int i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
 int main() {
-  cout << "Hello World!";
-  
+  int n;
+  cout << "Please enter a number: ";
+  cin >> n;
+  cout << sum(n);
   return 0;
 }
